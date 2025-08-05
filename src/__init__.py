f7bcbcd18b33a1509c2900b6b83952d9fa574c8b
Sayado01 <y.handane@gmail.com>
2025-08-05 15:13:13 +0200

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Housing MLOps Project
+A complete MLOps pipeline for California housing price prediction.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Yassine HANDANE"
+__email__ = "y.handane@gmail.com"
